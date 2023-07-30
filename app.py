4dfc0de61bb56f42002a1e965b920b9a6b2a1520
Dhaneswara Mandrasa <57479639+dhaneswaramandrasa@users.noreply.github.com>
2023-07-30 16:04:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,34 +21,34 @@ rfmTable['class_label_num'] = rfmTable['class_label'].map(label_mapping)
 # Set the title of the app
 st.title("RFM Dashboard")
 
-# Plot density plot of 'recency' using Plotly
+# Plot violin plot of 'recency' using Plotly
 fig_recency = go.Figure()
-fig_recency.add_trace(go.Histogram(x=rfmTable['recency'], histnorm='density', marker=dict(color='blue')))
-fig_recency.update_layout(title_text='Density Plot of Recency')
+fig_recency.add_trace(go.Violin(y=rfmTable['recency'], box_visible=True, meanline_visible=True, fillcolor='blue', line_color='blue'))
+fig_recency.update_layout(title_text='Violin Plot of Recency')
 
 # Show the Plotly figure using Streamlit
 st.plotly_chart(fig_recency)
 
-# Plot density plot of 'frequency' using Plotly
+# Plot violin plot of 'frequency' using Plotly
 fig_frequency = go.Figure()
-fig_frequency.add_trace(go.Histogram(x=rfmTable['frequency'], histnorm='density', marker=dict(color='green')))
-fig_frequency.update_layout(title_text='Density Plot of Frequency')
+fig_frequency.add_trace(go.Violin(y=rfmTable['frequency'], box_visible=True, meanline_visible=True, fillcolor='green', line_color='green'))
+fig_frequency.update_layout(title_text='Violin Plot of Frequency')
 
 # Show the Plotly figure using Streamlit
 st.plotly_chart(fig_frequency)
 
-# Plot density plot of 'actual_gmv' using Plotly
+# Plot violin plot of 'actual_gmv' using Plotly
 fig_actual_gmv = go.Figure()
-fig_actual_gmv.add_trace(go.Histogram(x=rfmTable['actual_gmv'], histnorm='density', marker=dict(color='orange')))
-fig_actual_gmv.update_layout(title_text='Density Plot of Actual GMV')
+fig_actual_gmv.add_trace(go.Violin(y=rfmTable['actual_gmv'], box_visible=True, meanline_visible=True, fillcolor='orange', line_color='orange'))
+fig_actual_gmv.update_layout(title_text='Violin Plot of Actual GMV')
 
 # Show the Plotly figure using Streamlit
 st.plotly_chart(fig_actual_gmv)
 
-# Plot density plot of 'gmv_missing_indicator' using Plotly
+# Plot violin plot of 'gmv_missing_indicator' using Plotly
 fig_gmv_missing = go.Figure()
-fig_gmv_missing.add_trace(go.Histogram(x=rfmTable['gmv_missing_indicator'], histnorm='density', marker=dict(color='red')))
-fig_gmv_missing.update_layout(title_text='Density Plot of GMV Missing Values')
+fig_gmv_missing.add_trace(go.Violin(y=rfmTable['gmv_missing_indicator'], box_visible=True, meanline_visible=True, fillcolor='red', line_color='red'))
+fig_gmv_missing.update_layout(title_text='Violin Plot of GMV Missing Values')
 
 # Show the Plotly figure using Streamlit
 st.plotly_chart(fig_gmv_missing)
