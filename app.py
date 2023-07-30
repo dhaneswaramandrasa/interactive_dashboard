98fc12d1bda251b8549edb2b66e55a7ebe6e2f63
Dhaneswara Mandrasa <57479639+dhaneswaramandrasa@users.noreply.github.com>
2023-07-30 14:07:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,24 @@ import pandas as pd
 import numpy as np
 
 # Sample data for demonstration
-data = {
-    'gmv_missing_indicator': np.random.rand(100),
-    'frequency': np.random.rand(100),
-    'actual_gmv': np.random.rand(100),
-    'class_kmeans_5': np.random.choice([1, 2, 3, 4, 5], size=100)
-}
-rfm_targeted = pd.DataFrame(data)
-
+rfmTable = pd.read_csv('data/rfmTable.csv')
+rfmTable['class_label'] = np.where(rfmTable['class_kmeans_5'] == 3, 'High Potential', (np.where(rfmTable['class_kmeans_5'] == 0, 'Medium Potential','Low Potential')))
 # Set the title of the app
 st.title("Plotly 3D Scatter Plot in Streamlit")
 
 # Create a 3D scatter plot with Plotly
 fig = go.Figure(data=go.Scatter3d(
-    x=np.log10(rfm_targeted['gmv_missing_indicator']),
-    y=np.log10(rfm_targeted['frequency']),
-    z=np.log10(rfm_targeted['actual_gmv']),
+    x=np.log10(rfmTable['recency']),
+    y=np.log10(rfmTable['frequency']),
+    z=np.log10(rfmTable['actual_gmv']),
     mode='markers',
-    marker=dict(size=8, color=rfm_targeted['class_kmeans_5'], colorscale='Viridis'),
-    text=rfm_targeted['class_kmeans_5']
+    marker=dict(size=8, color=rfmTable['class_label'], colorscale='Viridis'),
+    text=rfmTable['class_label']
 ))
 
 # Set labels for the axes
 fig.update_layout(scene=dict(
-    xaxis_title='gmv_missing_indicator',
+    xaxis_title='recency',
     yaxis_title='frequency',
     zaxis_title='actual_gmv'
 ))
